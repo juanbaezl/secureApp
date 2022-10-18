b333540bc69073cfe7254d2200d4dee0f0449f89
juanbaezl <juan.baez-l@mail.escuelaing.edu.co>
2022-10-18 13:24:37 -0500

    Finalizacion lab falta readme

--- src/main/java/co/edu/escuelaing/App.java
+++ src/main/java/co/edu/escuelaing/App.java
@@ -3,55 +3,54 @@ package co.edu.escuelaing;
 import static spark.Spark.*;
 import static co.edu.escuelaing.SecureURLReader.*;
 
-
 public class App {
     public static void main(String[] args) {
         port(getPort());
         secure(getKeyStore(), getKeyPwdStore(), null, null);
-        loadTrustStore(getTrustStore(),getKeyPwdStore());
-        get("hello", (req,res) -> "Hello " + getName() + "!");
-        get("remote", (req,res) -> readURL(getLink()));
+        loadTrustStore(getTrustStore(), getKeyPwdStore());
+        get("hello", (req, res) -> "Hello " + getName() + "!");
+        get("remote", (req, res) -> readURL(getLink()));
     }
 
     private static int getPort() {
         if (System.getenv("PORT") != null) {
             return Integer.parseInt(System.getenv("PORT"));
         }
-        return 5001;
+        return 5000;
     }
 
     private static String getKeyStore() {
         if (System.getenv("KS") != null) {
             return System.getenv("KS");
         }
-        return "";
+        return "keystores/ecikeystore.p12";
     }
 
     private static String getKeyPwdStore() {
         if (System.getenv("KSPWD") != null) {
             return System.getenv("KSPWD");
         }
-        return "";
+        return "eci123";
     }
 
     private static String getTrustStore() {
         if (System.getenv("TSTORE") != null) {
             return System.getenv("TSTORE");
         }
-        return "";
+        return "keystores/awscertifies.p12";
     }
 
     private static String getLink() {
         if (System.getenv("LINK") != null) {
             return System.getenv("LINK");
         }
-        return "";
+        return "https://ec2-3-82-150-130.compute-1.amazonaws.com:5000/hello";
     }
 
     private static String getName() {
         if (System.getenv("NAME") != null) {
             return System.getenv("NAME");
         }
-        return "";
+        return "World";
     }
 }
